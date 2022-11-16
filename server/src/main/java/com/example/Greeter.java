9adc8f4cae1b2fa3dcc0081147aee17617240b21
tayoru2 <tayoolokodana@yahoo.com>
2022-11-15 20:05:28 -0600

    fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,7 @@ public class Greeter {
    * This is a method.
    */
   public final String greet(final String someone) {
-    
+  
     return String.format("Hello Avinash, %s!", someone);
   }
 }
